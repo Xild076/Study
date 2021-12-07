a28e6f0690aecbca0973e1f862dd9d1f7dcd6f0f
cyber-bot1234 <64872070+cyber-bot1234@users.noreply.github.com>
2021-12-06 20:48:21 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import random
+
+
+class Study(object):
+    def __init__(self):
+        self.words = {
+            "I'll tell you":"contare^",
+            "knows":"conocen",
+            "the nickname":"el apodo",
+            "husband":"marido",
+            "says":"dice",
+            "don't know":"no se^",
+            "every day":"diario",
+            "the dances":"los biales",
+            "bottle":"botella",
+            "take off":"arranca",
+            "the band":"la banda",
+            "song":"cancio^n",
+            "searches/looks":"busca",
+            "dancer":"bailador",
+            "the people":"gente",
+            "begin":"empieza",
+            "yell":"gritar",
+            "nobody":"nadie",
+            "can":"puede",
+            "move":"mueve",
+            "better":"mejor",
+            "the rythem":"ri^tmo",
+            "dances":"todos",
+            "lose":"pierde",
+            "trot":"trote",
+        }
+
+        self.delete_copy = self.words
+
+        self.wrong = []
+
+    def start(self):
+        while len(self.delete_copy) != 0:
+            list_copy = list(self.delete_copy)
+            rand = random.randint(0, len(list_copy)-1)
+            print(rand)
+            object_guess = list_copy[rand]
+            guess = input(f"{object_guess}: ")
+            if guess == self.delete_copy[object_guess]:
+                print("Right")
+            else:
+                print("Wrong")
+            del (self.delete_copy[object_guess])
+
+if __name__ == "__main__":
+    study = Study()
+    study.start()
