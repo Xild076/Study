94494c081f666f2829831f9be33b6788ad7e57fe
cyber-bot1234 <64872070+cyber-bot1234@users.noreply.github.com>
2022-01-25 21:01:27 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
+
 import random
 
 
 class Study(object):
     def __init__(self):
         self.words = {
-            "promise":"prometo",
-            "alligence":"fidelidad",
-            "the flag":"la bandera",
-            "the United States":"los Estados Unidos",
-            "the republic":"la repu^blica",
-            "nation":"nacio^n",
-            "God":"Dio^s",
-            "indivisible":"indivisible",
-            "with":"con",
-            "liberty":"libertad",
-            "justice":"justicia",
-            "all":"todos"
+            "brother": "hermano",
+            "alma": "alma",
+            "really": "realmente",
+            "road": "camino",
+            "journey": "jornada",
+            "with me": "conmingo",
+            "even though": "aunque",
+            "man": "hombre",
+            "friendship": "amistad",
+            "respect": "respecto",
+            "affection": "carin~o",
+            "I remeber": "recuerdo",
+            "together": "juntos",
+            "hard": "duros",
+            "moments": "momentos",
+            "change": "cambiaste",
+            "strong": "fuertes",
+            "heart": "corazo^n",
+            "doors": "puertas",
+            "uncertain": "inciertas",
+            "life": "la vida",
+            "help": "ayude",
+            "find": "encontrar",
+            "exit": "salida",
+            "word": "palabra",
+            "certainly": "certeza",
+            "smile": "sonrisa",
+            "--": "abrazo",
+            "--": "cada",
+            "--": "llegada",
+            "you say": "dices",
+            "necessary": "preciso",
+            "feel": "sentir",
+            "big": "gran",
         }
 
         self.delete_copy = self.words
@@ -26,13 +49,14 @@ class Study(object):
         while len(self.delete_copy) != 0:
             list_copy = list(self.delete_copy)
             rand = random.randint(0, len(list_copy)-1)
-            print(rand)
+            print("")
             object_guess = list_copy[rand]
             guess = input(f"{object_guess}: ")
             if guess == self.delete_copy[object_guess]:
                 print("Right")
             else:
                 print("Wrong")
+                print(self.delete_copy[object_guess])
             del (self.delete_copy[object_guess])
 
 if __name__ == "__main__":
